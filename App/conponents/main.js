c28715407e95511befe8452fa0c9295c664ca684
Sharmake Ahmed <sharmakeahmedibrahim@gmail.com>
2024-01-26 16:03:52 +1300

    made small changes in css html and js filies

--- App/conponents/main.js
+++ App/conponents/main.js
@@ -1,11 +1,19 @@
-const hamburger = document.querySelector('.hamburger');
 
+function UserInput() {
+    const SearchBox = document.querySelector(".searchBox");
+    const invalid_txt = document.querySelector(".invalid-text")
+    const shortenBtn = document.querySelector(".shorten-btn")
 
+    console.log(SearchBox)
+    console.log(invalid_txt)
+    console.log(shortenBtn)
+ shortenBtn.addEventListener('click', ()=> {
+    if ( SearchBox.value === "")  {
+        invalid_txt.style.opacity = " 100";
+        SearchBox.classList.add('red-border');
+    }
 
-console.log(hamburger)
+ })
+}
 
-hamburger.addEventListener('click', ()=> {
-    const navBar = document.querySelector(".navbar");
-    navBar.classList.add('active');
-    console.log(navBar)
-})
+UserInput()
