b11e50cb774148eb9f7781942d86b8485b8b516a
Sharmake Ahmed <sharmakeahmedibrahim@gmail.com>
2024-01-28 00:35:09 +1300

    made small changes in html and js file last night

--- App/conponents/main.js
+++ App/conponents/main.js
@@ -4,7 +4,8 @@ function UserInput() {
     const SearchBox = document.querySelector(".searchBox");
     const invalid_txt = document.querySelector(".invalid-text")
     const shortenBtn = document.querySelector(".shorten-btn")
-
+    const links = document.querySelector(".links");
+    console.log(links);
 
     console.log(SearchBox)
     console.log(invalid_txt)
@@ -15,9 +16,10 @@ function UserInput() {
         SearchBox.classList.add('red-border');
     }
 
+
     else {
-        link_section.appendChild(links);
-        const links = document.createElement("div")
+       
+    
         const Ul = document.createElement("ul");
         const li = document.createElement("li");
         const Flex = document.createElement("div");
@@ -32,12 +34,15 @@ function UserInput() {
 
         links.classList.add('links');
         copyBtn.classList.add("copy-btn")
+        Flex.classList.add("flex");
+        copyBtn.classList.add("copy-btn")
         li.textContent = SearchBox.value;
         copyBtn.textContent = "Copy"
         aLink.textContent = SearchBox.value;
         copyBtn.addEventListener('click', ()=> {
             copyBtn.textContent = "Copied";
         })
+            
 
     }
   
@@ -48,3 +53,24 @@ function UserInput() {
 }
 
 UserInput()
+
+const url = 'https://ismaelc-bitly.p.rapidapi.com/v3/shorten?login=%3CREQUIRED%3E&apikey=%3CREQUIRED%3E&longUrl=http%3A%2F%2Fwww.mashape.com';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Host': 'ismaelc-bitly.p.rapidapi.com'
+	}
+};
+
+async function fetchData() {
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+    console.log(" something wrong")
+}
+
+}
