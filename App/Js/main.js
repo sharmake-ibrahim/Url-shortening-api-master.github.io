c7c89518bf9c02fed45b360d82ad0be8706570c5
Sharmake Ahmed <sharmakeahmedibrahim@gmail.com>
2024-02-13 00:09:57 +1300

    still working on input section's js and header section's css

--- App/Js/main.js
+++ App/Js/main.js
@@ -1,109 +1,101 @@
 
 
 
-const HamburgerMenu = ()=> {
-    const Ul = document.querySelector('ul');
-    const menu = document.querySelector('.menu');
-    menu.addEventListener("click", ()=> {
-        Ul.classList.toggle("show");
-    })
-}
+// const HamburgerMenu = ()=> {
+//     const Ul = document.querySelector('ul');
+//     const menu = document.querySelector('.menu');
+//     menu.addEventListener("click", ()=> {
+//         Ul.classList.toggle("show");
+//     })
+// }
 
-HamburgerMenu();
+// HamburgerMenu();
 
 
 
 function linkShortener() {
     
-    const allLinks = document.createElement('div')
-    allLinks.className = "allLinks";
-    const inputSection = document.createElement("div");
-    inputSection.className = "input-section";
-    const inputContainer = document.createElement("div");
-    inputContainer.className = "input-container";
-    const searchBox = document.createElement("div");
-    searchBox.className = "searchBox";
-    const input = document.createElement("input");
-    input.placeholder = "Shorten a link here..."
-    const errorTxt = document.createElement("p");
-    errorTxt.className = "error-txt";
-    errorTxt.textContent = "Please add a link";
-    const inputBtn = document.createElement('button');
-    inputBtn.className = " inputBtn";
-    inputBtn.textContent = "Shorten It!";
+                const allLinks = document.createElement('div')
+                allLinks.className = "allLinks";
+                const inputSection = document.createElement("div");
+                inputSection.className = "input-section";
+                const inputContainer = document.createElement("div");
+                inputContainer.className = "input-container";
+                const searchBox = document.createElement("div");
+                searchBox.className = "searchBox";
+                const input = document.createElement("input");
+                input.placeholder = "Shorten a link here..."
+                const errorTxt = document.createElement("p");
+                errorTxt.className = "error-txt";
+                errorTxt.textContent = "Please add a link";
+                const inputBtn = document.createElement('button');
+                inputBtn.className = " inputBtn";
+                inputBtn.textContent = "Shorten It!";
 
-    inputContainer.appendChild(searchBox);
-    searchBox.appendChild(input);
-    inputContainer.appendChild(errorTxt);
-    inputSection.appendChild(inputContainer);
-    inputSection.appendChild(inputBtn)
-
-
-    const inputLinks = document.createElement('div');
-    inputLinks.className = "inputLinks";
-    const linkOneTxt = document.createElement("p");
-    const aOne = document.createElement("a");
-    aOne.href= "https://github.com/Sharmake2024";
-    aOne.textContent = "https://github.com/Sharmake2024";
-    linkOneTxt.appendChild(aOne);
-    const linkContainer = document.createElement('div');
-    linkContainer.className = "link-container";
-    const list = document.createElement("li");
-    const aTwo = document.createElement('a');
-    aTwo.href= "https://github.com/Sharmake2024";
-    aTwo.textContent = "https://github.com/Sharmake2024"
-    list.appendChild(aTwo);
-    const linkBtn = document.createElement("button");
-    linkBtn.className = "linkBtn";
-    linkBtn.textContent = "Copy";
-    linkContainer.appendChild(list);
-    linkContainer.appendChild(linkBtn);
-
-    inputLinks.appendChild(linkOneTxt)
-    inputLinks.appendChild(linkContainer);
-
-    
-    allLinks.appendChild(inputSection);
-    allLinks.appendChild(inputLinks);
+                inputContainer.appendChild(searchBox);
+                searchBox.appendChild(input);
+                inputContainer.appendChild(errorTxt);
+                inputSection.appendChild(inputContainer);
+                inputSection.appendChild(inputBtn)
 
 
-    // inputBtn.addEventListener("click");
+                const inputLinks = document.createElement('div');
+                inputLinks.className = "inputLinks";
+                const linkOneTxt = document.createElement("p");
+                const aOne = document.createElement("a");
+                aOne.href= "https://github.com/Sharmake2024";
+                aOne.textContent = "https://github.com/Sharmake2024";
+                linkOneTxt.appendChild(aOne);
+                const linkContainer = document.createElement('div');
+                linkContainer.className = "link-container";
+                const list = document.createElement("li");
+                const aTwo = document.createElement('a');
+                aTwo.href= "https://github.com/Sharmake2024";
+                aTwo.textContent = "https://github.com/Sharmake2024"
+                list.appendChild(aTwo);
+                const linkBtn = document.createElement("button");
+                linkBtn.className = "linkBtn";
+                linkBtn.textContent = "Copy";
+                linkContainer.appendChild(list);
+                linkContainer.appendChild(linkBtn);
+
+                inputLinks.appendChild(linkOneTxt)
+                inputLinks.appendChild(linkContainer);
+
+                
+                allLinks.appendChild(inputSection);
+                allLinks.appendChild(inputLinks);
 
 
-// if( input.value === "") {
-//     searchBox.style.border = " 3px solid #F46363";
-//     errorTxt.style.display = "block";
-// }
- 
+                inputBtn.addEventListener("click", makeShortenUrl);
 
+                function makeShortenUrl() {
+                if( input.value === "") {
+                searchBox.style.border = " 3px solid #F46363";
+                errorTxt.style.display = "block";
+            }  else {
+                aOne.textContent = input.value;
 
-// async function getData(){
+                    async function getData() {
+                        const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${input.value}`);
+                        const data = await res.json();
 
-//     if( input.value === "") {
-//         searchBox.style.border = " 3px solid #F46363";
-//         errorTxt.style.display = "block";
-//     }
-//     else {
-    // const res = await fetch(`https://tinyurl.com/api-create.php?url=${input.value}`);
-    // const data = await res.json();
-    // console.log(data);
-    // console.log("Hello World");
- 
-    // }
- 
-    
+                        console.log(data);
 
-
-
-}
+                    }
+                    getData();
+            }
+            
+            return input.value = '';
+                
+            }
 
 
 
 
 
-    
 
-    return allLinks;
+                return allLinks;
 
 }
   
@@ -115,7 +107,3 @@ const share_link = document.querySelector(".share-link");
 
 share_link.append(linkShortener());
 
-
-
-
-// https://api.shrtco.de/v2/shorten?url=example.org/very/long/link.html
